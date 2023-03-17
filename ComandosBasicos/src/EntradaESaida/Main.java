4b6db11f6dd9e71f1ae98d2e970b88e28276c2ff
Michel Lopes Nascimento <125391379+MichelNascimento@users.noreply.github.com>
2023-03-17 17:28:09 -0300

    Update Main.java

--- ComandosBasicos/src/EntradaESaida/Main.java
+++ ComandosBasicos/src/EntradaESaida/Main.java
@@ -9,14 +9,14 @@ public class Main {
       //COMANDOS DE ENTRADA
       //======================================================================
       /*
-          Uma das possibilidades de entrada e sa�da de dados em Java � a 
-          utiliza��o da classe Scanner.
+          Uma das possibilidades de entrada e sai'da de dados em Java e' a 
+          utilizacao da classe Scanner.
       
-          Passos para a utiliza��o do Scanner:
+          Passos para a utilizacao do Scanner:
           
           1 - Importar o pacote java.util
           2 - Instanciar e criar um objeto Scanner
-          3 - Ler valores atrav�s do teclado
+          3 - Ler valores atrave's do teclado
       */
      
       //2 - Instanciar e criar um objeto Scanner
@@ -25,30 +25,30 @@ public class Main {
       int n = leitor.nextInt();
       float f = leitor.nextFloat();
       double d = leitor.nextDouble();
-      String s = leitor.next(); //L� apenas uma palavra, n�o � permitido o uso do caractere espa�o
+      String s = leitor.next(); //Le apenas uma palavra, nao e' permitido o uso do caractere espaco
       String ss = leitor.nextLine(); //Leitura de texto com mais de uma palavra
       
       /*
-          Como o m�todo next retorna um texto precisamos pegar o caractere da
-          primeira posi��o da palavra para isso usamos o m�todo charAt() na
-          posi��o zero
+          Como o me'todo next retorna um texto precisamos pegar o caractere da
+          primeira posicao da palavra para isso usamos o me'todo charAt() na
+          posicao zero
       */
       char c = leitor.next().charAt(0);
       //======================================================================
       
-      //COMANDOS DE SA�DA
+      //COMANDOS DE SAI'DA
       //======================================================================
       /*
-          Existem algumas op��es de comandos de sa�da utilizando a classe 
-          System. S�o elas: 
+          Existem algumas opcoes de comandos de sai'da utilizando a classe 
+          System. Sao elas: 
           
-          1 - print() - Imprime o valor de uma vari�vel
-          2 - printf() - Imprime o valor de uma vari�vel conforme uma formata��o especificada
-          3 - println() - Imprime o valorm de uma vari�vel e pula uma linha
+          1 - print() - Imprime o valor de uma varia'vel
+          2 - printf() - Imprime o valor de uma varia'vel conforme uma formatacao especificada
+          3 - println() - Imprime o valorm de uma varia'vel e pula uma linha
       */
       
-      String texto = "Esse texto ser� impresso no console";
-      String outroTexto = "Esse texto tamb�m ser� impresso no console";
+      String texto = "Esse texto sera' impresso no console";
+      String outroTexto = "Esse texto tambe'm sera' impresso no console";
       System.out.println(texto);
       System.out.print(outroTexto);
       //======================================================================
